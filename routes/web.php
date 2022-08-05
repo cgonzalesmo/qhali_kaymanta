d6995b01c2d481be8e3604d393effcc52e47b280
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2022-08-05 01:43:16 -0500

    add model tomografia, add controllers (tomografia, user, rol).

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//agregamos controladores
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\TomografiaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +27,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'],function(){
+	Route::resource('roles',RolController::class);
+	Route::resource('usuarios',UsuarioController::class);
+	Route::resource('tomografias',TomografiaController::class);
+});
